d231a81e4711f59da87e3de6ccb93151b648d41a
Peter Stenger <peter.a.stenger@gmail.com>
2019-06-17 15:11:15 -0400

    Add files

---
+++ hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import colors
+print("Hello World!")
+print(colors.multi("hello world!"))
---
+++ nyan.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import time
+import colors as c
+while True:
+    print(c.random() + "Joey is awesome",end="")
+    time.sleep(1)
