4ab1ef9e6427b5e45a02f88f5359c845ed320fe8
Joey <jorabrar2007@gmail.com>
2019-06-19 13:14:47 -0500

    Added Dice Program

--- hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import colors
-print("Hello World!")
-print(colors.multi("hello world!"))
--- hi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import colors as c
-name = input(c.blue + "What is your name")
-age = input(c.magenta + "What is your age")
-print(age)
-num_age = int(age) #this is the what the age is
-#print(c.red + name + c.x + " is going to be", num_age+1,"years old in a year.") 
-print("{} {} {} {}" .format(name, "is going to be", num_age+5, "years old in 5 years"))
